5865262c0cb17ae414c52f33abf538674600359c
Sockmonkey11 <goodcupcakes344@gmail.com>
2025-09-03 18:03:45 -0400

    Remove ignored files from tracking

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/hospitalfinder/obj/local/arm64-v8a/objs-debug/webpdecoder_static/src/utils/random_utils.o.d
+++
@@ -1,8 +0,0 @@
-/mnt/c/Users/emman/Desktop/moneymaker/cloudGamingManager/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/webpdecoder_static/src/utils/random_utils.o: \
-  /mnt/c/Users/emman/Desktop/moneymaker/cloudGamingManager/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/utils/random_utils.c \
-  /mnt/c/Users/emman/Desktop/moneymaker/cloudGamingManager/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/utils/random_utils.h \
-  /mnt/c/Users/emman/Desktop/moneymaker/cloudGamingManager/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h
-
-/mnt/c/Users/emman/Desktop/moneymaker/cloudGamingManager/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/utils/random_utils.h:
-
-/mnt/c/Users/emman/Desktop/moneymaker/cloudGamingManager/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h:
